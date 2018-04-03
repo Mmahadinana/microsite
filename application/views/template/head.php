0345b9fe6f22754e93c18634c83f6f495c35fa77
Nkhethoa <nkhethoa@gmail.com>
2018-04-03 12:59:49 +0100

    template build

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -1,8 +1,19 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <head>
+	<title>RGDP Portugal</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    
+	<?php 
+    $cssFiles =['bootstrap.min.css',
+                'font-awesome.min.css',
+    			'micro.css',
+    			];
+    foreach ($cssFiles as $cssFile) {?>
+        <link href='<?php echo base_url("assets/css/$cssFile")?>' rel="stylesheet">
+    <?php }?>
+
 </head>
