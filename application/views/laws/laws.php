fbc442f66bd67de913439f815e6bad0913ca7730
Rosy Marumo <rosy.marumo@grupoacin.local>
2018-04-03 12:57:24 +0100

    update view

--- application/views/laws/laws.php
+++ application/views/laws/laws.php
@@ -1,17 +1,17 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!--main body-->
-<body>
+
 
 	<!--landing view-->
-	<div id="splash"></div>
+	<section id="splash" class="container"></section>
 
 	<!--main body-->
-	<div id="home"></div>
+	<section id="home" class="container"></section>
 
 	<!--main body-->
-	<div id="about"></div>
+	<section id="about" class="container"></section>
 
 	<!--main body-->
-	<div id="laws"></div>
-	
-</body>
+	<section id="laws" class="container"></section>
+
+
