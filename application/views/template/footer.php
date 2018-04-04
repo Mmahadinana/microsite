6a9700611978c5a908386d3dbd39d663faaf2230
nkhethoa <nkhethoa@gmail.com>
2018-04-04 08:37:25 +0100

    microsite

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,1 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<?php 
+	$jsFiles=['bootstrap.min.js',
+
+			 ];
+	foreach ($jsFiles as $jsFile) {?>
+        <script src='<?php echo base_url("assets/js/$jsFile")?>'></script>
+    <?php }?> 
